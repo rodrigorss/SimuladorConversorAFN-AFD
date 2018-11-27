bb9d0240d0fb6b91893d8099201246feddaeb5a2
rodrigorss <rodrigo.silva.014@acad.pucrs.br>
2018-11-27 17:25:49 -0200

    Corrige leitura

--- src/App.java
+++ src/App.java
@@ -1,15 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import dk.brics.automaton.Automaton;
-import dk.brics.automaton.StatePair;
 import dk.brics.automaton.Transition;
 
 public class App {
@@ -23,20 +21,25 @@ public class App {
 		}
 		String linha[] = sc.nextLine().split("=");
 		String nomeAutomato = linha[0];
-		String linhaSplit[] = linha[1].substring(1, linha[1].length() - 1).split(",");
-		System.out.println(Arrays.toString(linhaSplit));
+		Matcher linhaMatcher = Pattern.compile("\\((\\{.*\\}),(\\{.*\\}),(\\w+),(\\{.*\\})\\)").matcher(linha[1]);
+		linhaMatcher.matches();
+		System.out.println("Nome: " + nomeAutomato);
+		System.out.println("Infos: " + linhaMatcher.group());
+		System.out.println("Estados: " + linhaMatcher.group(1));
+		System.out.println("Estados Finais: " + linhaMatcher.group(4));
+		System.out.println("Estado Inicial: " + linhaMatcher.group(3));
+		System.out.println("Alfabeto: " + linhaMatcher.group(2));
 		List<String> listaEstados = new ArrayList<>();
-		String sEstados = linhaSplit[0];
-		System.out.println(linha);
-		for (String estado : sEstados.substring(0, sEstados.length() - 1).split(","))
+		String sEstados = linhaMatcher.group(1);
+		for (String estado : sEstados.substring(1, sEstados.length() - 1).split(","))
 			listaEstados.add(estado);
 		List<String> estadosFinais = new ArrayList<>();
-		String sEstadosFinais = linhaSplit[3];
-		for (String estado : sEstadosFinais.substring(1, sEstadosFinais.length()).split(","))
+		String sEstadosFinais = linhaMatcher.group(4);
+		for (String estado : sEstadosFinais.substring(1, sEstadosFinais.length() - 1).split(","))
 			estadosFinais.add(estado);
-		String estadoInicial = linhaSplit[2];
+		String estadoInicial = linhaMatcher.group(3);
 		List<String> alfabeto = new ArrayList<>();
-		String sAlfabeto = linhaSplit[1];
+		String sAlfabeto = linhaMatcher.group(2);
 		for (String letra : sAlfabeto.substring(1, sAlfabeto.length() - 1).split(","))
 			alfabeto.add(String.valueOf(letra.charAt(0)));
 		Automaton aut = new Automaton();
@@ -47,26 +50,17 @@ public class App {
 		sc.nextLine(); // Pula "Prog"
 		while (sc.hasNext()) {
 			String l = sc.nextLine();
-			String estadoOrigem = Pattern.compile("\\((.*),").matcher(l).group(1);
-			String simbolo = Pattern.compile("\\(.*,(.*)\\)").matcher(l).group(1);
-			String estadoDestino = Pattern.compile("=(.*)").matcher(l).group(1);
+			System.out.println("Linha: " + l);
+			String valores[] = l.replaceAll("\\(|\\)", "").replaceAll("=", ",").split(",");
+			String estadoOrigem = valores[0];
+			String simbolo = valores[1];
+			String estadoDestino = valores[2];
+			System.out.println("Estado Origem: " + estadoOrigem);
+			System.out.println("Símbolo: " + simbolo);
+			System.out.println("Estado Destino: " + estadoDestino);
 			Transition transicao = new Transition(Character.MIN_VALUE, Character.MAX_VALUE, estados.get(estadoDestino));
 			estados.get(estadoOrigem).addTransition(transicao);
 		}
-		ArrayList<StatePair> pares = new ArrayList<>();
-		for (Estado e : estados.values()) {
-			Set<Transition> trans = e.getTransitions();
-
-			int[] count = new int[trans.size()];
-			for (int i = 0; i < count.length; i++)
-				if (count[i] > 1) {
-					Transition t = (Transition) (trans.toArray()[i]);
-					pares.add(new StatePair(e, t.getDest()));
-				}
-		}
-		aut.addEpsilons(pares);
-		aut.setDeterministic(false);
-		System.out.println(aut.toDot());
 	}
 
 }
