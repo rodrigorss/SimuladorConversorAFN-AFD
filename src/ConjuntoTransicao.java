551bcec46e2eb2d63790e4771cc142993f59d73b
rodrigorss <rodrigo.silva.014@acad.pucrs.br>
2018-11-28 05:28:44 -0200

    Altera pra equals a comparação

--- src/ConjuntoTransicao.java
+++ src/ConjuntoTransicao.java
@@ -1,43 +1,44 @@
 
-
 public class ConjuntoTransicao {
-	
+
 	private ConjuntoEstados conjEstadosOrigem;
 	private String simboloLido;
 	private ConjuntoEstados conjEstadosResult;
-	
+
 	public ConjuntoTransicao(ConjuntoEstados conjEstadosOrigem, String simbolo, ConjuntoEstados conjEstadosResult) {
 		this.conjEstadosOrigem = conjEstadosOrigem;
 		this.simboloLido = simbolo;
 		this.conjEstadosResult = conjEstadosResult;
 	}
-	
+
 	public ConjuntoEstados getConjEstadosOrigem() {
 		return conjEstadosOrigem;
 	}
-	
+
 	public String getSimboloLido() {
 		return simboloLido;
 	}
-	
+
 	public ConjuntoEstados getConjEstadosResult() {
 		return conjEstadosResult;
 	}
-	
-	public String toString() {
-		//return conjEstadosOrigem.getNome();
-		return "("+conjEstadosOrigem+","+simboloLido+") = "+conjEstadosResult;
-	}
-	
+
 	@Override
-	public boolean equals(Object bagulho) {
-		ConjuntoTransicao conj = ((ConjuntoTransicao)bagulho);
-		return conj.getConjEstadosOrigem()==this.getConjEstadosOrigem() && conj.getSimboloLido()==this.getSimboloLido() 
-				&& conj.getConjEstadosResult() == this.getConjEstadosResult();
+	public String toString() {
+		// return conjEstadosOrigem.getNome();
+		return "(" + conjEstadosOrigem + "," + simboloLido + ") = " + conjEstadosResult;
 	}
 
-	/*@Override
+	@Override
+	public boolean equals(Object bagulho) {
+		ConjuntoTransicao conj = (ConjuntoTransicao) bagulho;
+		return conj.getConjEstadosOrigem().equals(this.getConjEstadosOrigem())
+				&& conj.getSimboloLido().equals(this.getSimboloLido())
+				&& conj.getConjEstadosResult().equals(this.getConjEstadosResult());
+	}
+
+	@Override
 	public int hashCode() {
-		return getNome().hashCode();
-	}*/
+		return toString().hashCode();
+	}
 }
