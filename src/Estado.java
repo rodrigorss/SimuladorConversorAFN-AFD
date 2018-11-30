333491fd5fda155b679328f8ce45c660daf70463
matheus zanon <matheus.zanon@acad.pucrs.br>
2018-11-30 19:57:35 -0200

    Método p/ verificar palavra, precisa testes.

--- src/Estado.java
+++ src/Estado.java
@@ -1,11 +1,48 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Estado {
-	
 	private String nome;
-	
-	public Estado(String nome) {
+	private Map<String, List<Estado>> transicoes;
+	private boolean estadoFinal;
+
+	public Estado(String nome, boolean estadoFinal) {
 		this.nome = nome;
+		this.estadoFinal = estadoFinal;
+		transicoes = new HashMap<>();
 	}
-	
-	public String getNome() { return nome;}
+
+	public Map<String, List<Estado>> getTransicoes() {
+		//return Collections.unmodifiableMap(transicoes);
+		return transicoes;
+	}
+
+	public void adicionarTransicao(String simbolo, List<Estado> estados) {
+		transicoes.put(simbolo, estados);
+	}
+
+	public boolean isEstadoFinal() {
+		return estadoFinal;
+	}
+
+	public void setEstadoFinal(boolean estadoFinal) {
+		this.estadoFinal = estadoFinal;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return ((Estado) obj).getNome().equals(nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return nome.hashCode();
+	}
+
 }
